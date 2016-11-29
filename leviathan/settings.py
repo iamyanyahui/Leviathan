cc3acb4e186859e86a82ebdaf9e2776b93bab921
yanyahui <17801006099@163.com>
2016-11-29 14:18:29 +0800

    修改settings中数据库配置

--- leviathan/settings.py
+++ leviathan/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
 	    'ENGINE': 'django.db.backends.mysql',
         'NAME': 'leviathan',
         'USER': 'leviathan',
-        'PASSWORD': 'passwd',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
 
